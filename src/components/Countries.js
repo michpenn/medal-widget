cadfacd9a8eea848d73cb527683418216c321ed1
michpenn <michpenn91@gmail.com>
2018-12-17 17:24:22 -0500

    only show ten countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -7,7 +7,7 @@ class Countries extends Component {
     return (
       <Consumer>
         {value => {
-          const countries = value.countries;
+          const countries = value.countries.slice(0,10);
           return (
             <React.Fragment>
               {countries.map((country, index) => (
@@ -18,14 +18,6 @@ class Countries extends Component {
         }}
       </Consumer>
     );
-    // const { countries } = this.props;
-    // return (
-    //   <React.Fragment>
-    //     {countries.map((country, index) => (
-    //       <Country key={country.code} index={index} country={country} />
-    //     ))}
-    //   </React.Fragment>
-    // );
   }
 }
 
